d80bafdf6d4b666d0456d70f3a35f1f59dbd0c62
jadenma <83566257+jadenma@users.noreply.github.com>
2025-03-27 22:47:30 -0700

    Update Map.java

--- P2_first_last/src/Map.java
+++ P2_first_last/src/Map.java
@@ -4,61 +4,55 @@ import java.util.Scanner;
 
 public class Map {
 
-	private Tile[][] map; 
+	private Tile[][] map;
+
+	public Map(int row, int column) {
+		
+		map = new Tile[row][column];
+		
+		for (int i = 0; i < row; i++) {
+			
+			for (int j = 0; j < column; j++) {
+				
+				map[i][j] = new Tile(i, j, '.'));
+				
+			}
+		}
+	}
 	
-	private int numRows, numCols, numRooms;
-    private Tile start, goal;
+	public void set(int row, int column, Tile tile) {
+		
+		map[row][column] = tile;
+		
+	}
+	
+	public Tile get(int row, int column) {
+		
+		//check boundaries
+		if (row < 0 || column < 0 || row >= map.length || column >= map[0].length) {
+			
+			return new Tile(row, column, '@');
+			
+		}
+		
+		return map[row][column];
+		
+	}
+	
+	public void print() {
+		for (int i = 0; i < map.length; i++) {
+			
+			for (int j = 0; j < map[i].length; j++) {
+				
+				System.out.print(map[i][j].getType());
 
-    public Map(String filename) {
-        readMap(filename);
-    }
+			}
+			
+			System.out.println();
+			
+		}
+	}
     
-    private void readMap(String filename) {
-        try {
-            File file = new File(filename);
-            Scanner scanner = new Scanner(file);
-            numRows = scanner.nextInt();
-            numCols = scanner.nextInt();
-            numRooms = scanner.nextInt();
-            scanner.nextLine();
-
-            map = new Tile[numRows][numCols];
-
-            for (int row = 0; row < numRows; row++) {
-                String line = scanner.nextLine();
-                for (int col = 0; col < Math.min(numCols, line.length()); col++) {
-                    char type = line.charAt(col);
-                    map[row][col] = new Tile(row, col, type);
-
-                    if (type == 'S') start = map[row][col];
-                    if (type == 'G') goal = map[row][col];
-                }
-            }
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found: " + filename);
-        }
-    }
-    
-    public Tile[][] getMap() {
-        return map;
-    }
-
-    public int getNumRows() {
-        return numRows;
-    }
-
-    public int getNumCols() {
-        return numCols;
-    }
-
-    public Tile getStart() {
-        return start;
-    }
-
-    public Tile getGoal() {
-        return goal;
-    }
 	
 	
 }
