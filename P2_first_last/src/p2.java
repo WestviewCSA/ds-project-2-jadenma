6bbd773b9ee09ad4f7443c412bce8c8bccf7fd3d
jadenma <83566257+jadenma@users.noreply.github.com>
2025-03-27 23:47:59 -0700

    fixed readMap

--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -1,39 +1,58 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class p2 {
 
+	static int[] colDir = {0, 1, 0, -1};
+	static int[] rowDir = {1, 0, -1, 0};
+	static Map[] grid;
+	static LinkedList<Tile> path;
+	static int numRows;
+	static int numCols;
+	static int numRooms;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("p2");
-		
-		readMap("Design 4");
+		System.out.println("Testing with map file: " + filename);
+		readMap("Design 5");
+		queueSolve();
 	}
 	
 	public static void readMap(String filename) {
-	    File file = new File(filename);
-	    Scanner scanner;
+	    
 	    try {
 	        scanner = new Scanner(file);
-	        int numRows = scanner.nextInt();
-	        int numCols = scanner.nextInt();
-	        int numRooms = scanner.nextInt();
-	        scanner.nextLine();  // Move to the next line after reading dimensions
+	        numRows = scanner.nextInt();
+	        numCols = scanner.nextInt();
+	        numRooms = scanner.nextInt();
+		    File file = new File(filename);
+	    Scanner scan = new Scanner(file);
 
-	        Tile[][] map = new Tile[numRows][numCols];
+
+	        grid = new Map[numRooms];
 
 	        int rowIndex = 0;
-	        while (scanner.hasNextLine() && rowIndex < numRows) {
-	            String row = scanner.nextLine();
-	            if (row.length() > 0) {
-	                for (int i = 0; i < numCols && i < row.length(); i++) {
-	                    char e1 = row.charAt(i);
-	                    map[rowIndex][i] = new Tile(rowIndex, i, e1);
-	                }
-	                rowIndex++;
-	            }
-	        }
+	        for (int i = 0; i < numRooms; i++) {
+			
+			grid[i] = new Map(numRows, numCols);
+			
+			while (scanner.hasNextLine() && rowIndex < numRows) {
+				String row = scanner.nextLine();
+				//check if something is on the next line
+				if (row.length() > 0) {
+					for (int j = 0; j < numCols && j < row.length(); j++) {
+						char a = row.charAt(j);
+						Tile tile = new Tile(rowIndex, j, a);
+						grid[i].set(rowIndex, i, obj);
+					}
+					rowIndex++;
+				}
+			}
+		}
 	        scanner.close();
 
 	        // For debugging, print the map
