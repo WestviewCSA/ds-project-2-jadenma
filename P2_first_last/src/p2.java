d892aa223d999f8356c5846ea20ca6259aa4fe3c
jadenma <83566257+jadenma@users.noreply.github.com>
2025-03-28 00:47:58 -0700

    finished queueSolve

--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -62,8 +62,73 @@ public class p2 {
 	    }
 	}
 	
-	
-	
-	
+	public static void queueSolution {
+
+		for (int i = 0; i < numRooms; i++) {
+
+			Queue<Tile> queue = new LinkedList<>();
+			boolean[][] visited = new boolean[numRows][numCols];
+			int distance[][] = new int[numRows][numCols];
+
+			for (int j = 0; j < numRows; j++) {
+				for (int k = 0; k < numCols; k++) {
+					distance[i][j] = -1;
+					if (grid[room].get(j, k).getType() == 'W') {
+						queue.add(grid[i].get(j, k));
+						visited[j][k] = true;
+						distance[j][k] = 0;
+					}
+				}
+			}
+			while (!queue.isEmpty()) {
+	            		Tile current = queue.remove();
+	            		int curRow = current.getRow();
+	            		int curCol = current.getCol();
+
+	            		//check target reached
+	           		if (grid[i].get(curRow, curCol).getType() == '$' || grid[i].get(curRow, curCol).getType() == '|') {
+
+	                		LinkedList<Tile> path = new LinkedList<>();
+	                		Tile targetTile = current;
+	                
+	                		while (targetTile != null) {
+	                    			path.addFirst(targetTile); //reverse list
+	                    			targetTile = parent[targetTile.getRow()][targetTile.getCol()];
+	                		}
+	                
+	                		//mark the path
+	                		for (Tile t : path) {
+	                    			if (t.getType() == '.') {
+	                        		t.setType('+'); 
+	                    		}
+	                	}
+	                
+	                	System.out.println("Path found:");
+	                	grid[i].print();  //Print the grid with the path marked
+	                	return;
+	            	}
+
+	            //4 directions
+	            for (int direction = 0; direction < 4; direction++) {
+	                int nextRow = curRow + colDir[direction];
+	                int nextCol = curCol + rowDir[direction];
+
+	                if (nextRow >= 0 && nextCol >= 0 && nextRow < numRows && nextCol < numCols && !visited[nextRow][nextCol] && grid[i].get(nextRow, nextCol).getType() != '@') {
+	                    visited[nextRow][nextCol] = true;
+	                    parent[nextRow][nextCol] = currentTile;  //set the parent
+	                    queue.add(grid[i].get(nextRow, nextCol));
+	                }
+	            }
+	        }
+		}
+		//if the target was not found
+	        System.out.println("Target not found.");
+		
+	}
+
+	public static void stackSolve() {
+
+		
+	}
 }
 
